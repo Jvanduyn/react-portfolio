ffa3ff86c6aff005fb15eaed3feac876aad661f6
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2023-04-03 13:37:01 -0400

    initial commit

---
+++ src/components/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Jacob VanDuyn</h1>
+            <p>
+            </p>
+        </div>
+    );
+}
