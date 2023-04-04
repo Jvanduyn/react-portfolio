b1487a3848e0e820f4335dcf83ab36aa1c210f7c
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2023-04-03 21:53:11 -0400

    update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,8 +4,6 @@ export default function Resume() {
     return (
         <div style={{ display: 'flex', justifyContent: 'center' }}>
 
-            {/* <img src={resume}></img> */}
-
         </div>
     );
 }
